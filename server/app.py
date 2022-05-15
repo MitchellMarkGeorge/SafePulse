09e50d99785c5db54bc9be0dfb0343633cebd20a
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2022-05-15 13:54:02 -0400

    pushed up server code and used enviroment variables

---
+++ server/app.py
@@ -0,0 +1,49 @@
+from twilio.rest import Client
+import requests, json, os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+#verification stuff
+ACCOUNT_SID = os.environ["ACCOUNT_SID"]
+AUTH_TOKEN = os.environ["AUTH_TOKEN"]
+
+#string constants
+STR_START = '<Response><Say>'
+STR_END = '</Say></Response>'
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN) 
+
+app = Flask(__name__)
+CORS(app)
+
+#make call to authorities with user information
+def make_call(voice_message, user_num, client):
+    call = client.calls.create(
+                                twiml = voice_message, 
+                                to='+16479900182', 
+                                from_= '+1' + str(user_num))
+    print(call.sid)
+
+#read JSON information, get information
+@app.route('/call', methods=['POST'])
+def read():
+    request_json = request.get_json()
+    user_name = request_json['name']
+    user_num = request_json['phone'] 
+    user_address = request_json['address']
+
+    if "access_information" in request_json:
+        user_access = request_json['access_information']
+        voice_message = str(STR_START + 'hello my name is ' + user_name + 'I am overdosing at ' + user_address + 'please send help' + user_access  + STR_END)
+    else:
+        voice_message = str(STR_START + 'hello my name is ' + user_name + 'I am overdosing at ' + user_address + 'please send help' + STR_END)
+    try:    
+        make_call(1)
+    except:
+        print("Call was not able to be made")
+        return json.dumps({'Error':True}), 403, {'ContentType':'application/json'} 
+    return json.dumps({'Success':True}), 200, {'ContentType':'application/json'} 
+    
